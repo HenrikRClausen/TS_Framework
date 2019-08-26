f7287e53d95eeae34238fc21085601b0f8d85a72
Henrik Ræder Clausen <henrik@localhost.localdomain>
2019-08-26 22:58:00 +0200

    Første commit

--- Source/index.ts
+++
@@ -1,9 +0,0 @@
-import { Bruger } from "./Models/Bruger";
-
-const bruger = new Bruger({ navn: "Frederikke", alder: 21 });
-
-bruger.begivenheder.on("skift", () => {
-  console.log("Skiftetid!");
-});
-
-bruger.begivenheder.trigger("skift");
